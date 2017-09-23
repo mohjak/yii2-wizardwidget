8788ff68ba05cd58f69365b3c63ef18969bd0c2b
Mohjak <mohjak@gmail.com>
2017-09-23 12:08:42 +0300

    ✨  Apply header and step lines layout.

--- WizardWidget.php
+++ WizardWidget.php
@@ -10,210 +10,253 @@ use yii;
 use yii\base\Widget;
 use yii\web\View;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
  * Widget for wizard widget
  *
  * @author A.F.Schuurman <andre.schuurman+yii2-wizardwidget@gmail.com>
  * @since 1.0
+ * @author Mohammad Jaqmaqji <mohjak@gmail.com>
+ * @since 1.2.3
  */
 class WizardWidget extends Widget {
 
-	/**
-	 * @var string widget html id
-	 */
-	public $id = 'wizard';
+    /**
+     * @var string widget html id
+     */
+    public $id = 'wizard';
 
-	/**
-	 * @var array default button configuration
-	 */
-	public $default_buttons = [
-		'prev' => ['title' => 'Previous', 'options' => [ 'class' => 'btn btn-default', 'type' => 'button']],
-		'next' => ['title' => 'Next', 'options' => [ 'class' => 'btn btn-default', 'type' => 'button']],
-		'save' => ['title' => 'Save', 'options' => [ 'class' => 'btn btn-default', 'type' => 'button']],
-		'skip' => ['title' => 'Skip', 'options' => [ 'class' => 'btn btn-default', 'type' => 'button']],
-	];
+    /**
+     * @var array default button configuration
+     */
+    public $default_buttons = [
+        'prev' => ['title' => 'Previous', 'options' => [ 'type' => 'button' ]],
+        'next' => ['title' => 'Next', 'options' => [ 'type' => 'button' ]],
+        'save' => ['title' => 'Save', 'options' => [ 'type' => 'button' ]],
+        'skip' => ['title' => 'Skip', 'options' => [ 'type' => 'button' ]],
+    ];
 
-	/**
-	 * @var array the wizard step definition
-	 */
-	public $steps = [];
+    /**
+     * @var array the wizard step definition
+     */
+    public $steps = [];
 
-	/**
-	 * @var integer step id to start with
-	 */
-	public $start_step = null;
+    /**
+     * @var integer step id to start with
+     */
+    public $start_step = null;
 
-	/**
-	 * @var string optional final complete step content
-	 */
-	public $complete_content = '';
+    /**
+     * @var string optional final complete step content
+     */
+    public $complete_content = '';
 
-	/**
-	 * Main entry to execute the widget
-	 */
-	public function run() {
-		parent::run();
-		WizardWidgetAsset::register($this->getView());
+    public $campaign_name = '';
 
+    /**
+     * Main entry to execute the widget
+     */
+    public function run() {
+        parent::run();
+        WizardWidgetAsset::register($this->getView());
 
-		// Wizard line calculation
-		$step_count = count($this->steps)+($this->complete_content?1:0);
-		$wizard_line_distribution = round(100 / $step_count); // Percentage
-		$wizard_line_width = round(100 - $wizard_line_distribution); // Percentage
-		$wizard_line = '';
+        // Wizard line calculation
+        // $step_count = count($this->steps)+($this->complete_content?1:0);
+        // $wizard_line_distribution = round(100 / $step_count); // Percentage
+        // $wizard_line_width = round(100 - $wizard_line_distribution); // Percentage
+        $wizard_line = '';
 
-		$tab_content = '';
+        $name_line = '';
 
-		// Navigation tracker
-		end($this->steps);
-		$last_id = key($this->steps);
+        $tab_content = '';
 
-		$first = true;
-		$class = '';
+        // Navigation tracker
+        end($this->steps);
+        $last_id = key($this->steps);
 
-		foreach ($this->steps as $id => $step) {
+        $first = true;
+        $class = '';
+        $step_navigation_area = '';
 
-			// Current or fist step is active, next steps are inactive (previous steps are available)
-			if ($id == $this->start_step or (is_null($this->start_step) && $class == '')) {
-				$class = 'active';
-			} elseif ($class == 'active') {
-				$class = 'disabled';
-			}
+        foreach ($this->steps as $id => $step) {
 
-			// Add icons to the wizard line
-			$wizard_line .= Html::tag(
-				'li',
-				Html::a('<span class="round-tab"><i class="'.$step['icon'].'"></i></span>', '#step'.$id, [
-					'data-toggle' => 'tab',
-					'aria-controls' => 'step'.$id,
-					'role' => 'tab',
-					'title' => $step['title'],
-				]),
-				array_merge(
-					[
-						'role' => 'presentation',
-						'class' => $class,
-						'style' => ['width' => $wizard_line_distribution.'%']
-					],
-					isset($step['options']) ? $step['options'] : []
-				)
-			);
+            // Current or fist step is active, next steps are inactive (previous steps are available)
+            if ($id == $this->start_step or (is_null($this->start_step) && $class == '')) {
+                $class = 'active';
+            } elseif ($class == 'active') {
+                $class = 'disabled';
+            }
 
-			// Setup tab content
-			$tab_content .= '<div class="tab-pane '.$class.'" role="tabpanel" id="step'.$id.'">';
-			$tab_content .= $step['content'];
+            // Add icons to the wizard line
+            $wizard_line .= Html::tag(
+                'li',
+                Html::a($step['title'], '#step'.$id, [
+                    'data-toggle' => 'tab',
+                    'aria-controls' => 'step'.$id,
+                    'role' => 'tab',
+                    'title' => $step['title'],
+                ]),
+                array_merge(
+                    [
+                        'role' => 'presentation',
+                        'class' => $class,
+                        // 'style' => ['width' => $wizard_line_distribution.'%']
+                    ],
+                    isset($step['options']) ? $step['options'] : []
+                )
+            );
 
-			// Setup navigation buttons
-			$buttons = [];
-			$button_id = "{$this->id}_step{$id}_";
-			if (!$first) {
-				// Show previous button except on first step
-				$buttons[] = $this->navButton('prev', $step, $button_id);
-			}
-			if (array_key_exists('skippable', $step) and $step['skippable'] === true) {
-				// Show skip button if specified
-				$buttons[] = $this->navButton('skip', $step, $button_id);
-			}
-			if ($id == $last_id) {
-				// Show save button on last step
-				$buttons[] = $this->navButton('save', $step, $button_id);
-			} else {
-				// On all previous steps show next button
-				$buttons[] = $this->navButton('next', $step, $button_id);
-			}
-			// Add buttons to tab content
-			$tab_content .= Html::ul($buttons, ['class' => 'list-inline pull-right', 'encode' => false]);
+            $wizard_line .= '>';
 
-			// Finish tab
-			$tab_content .= '</div>';
+            // Setup tab content
+            $tab_content .= '<div class="main tab-pane '.$class.'" role="tabpanel" id="step'.$id.'">';
 
-			$first = false;
-		}
+            // Setup navigation buttons
+            $buttons = [];
+            $button_id = "{$this->id}_step{$id}_";
+            if (!$first) {
+                // Show previous button except on first step
+                $buttons[] = $this->navButton('prev', $step, $button_id);
+            }
+            if (array_key_exists('skippable', $step) and $step['skippable'] === true) {
+                // Show skip button if specified
+                $buttons[] = $this->navButton('skip', $step, $button_id);
+            }
+            if ($id == $last_id) {
+                // Show save button on last step
+                $buttons[] = $this->navButton('save', $step, $button_id);
+            } else {
+                // On all previous steps show next button
+                $buttons[] = $this->navButton('next', $step, $button_id);
+            }
 
-		// Add a completed step if specified
-		if ($this->complete_content) {
-			$class = 'disabled';
+            // Add buttons to tab content
+            $step_navigation_area .= '<div class="step-navigation-area">';
+            $step_navigation_area .= '<h1 class="step-description">Select Recipients List</h1>';
+            $step_navigation_area .= Html::ul($buttons, ['class' => 'step-navigation', 'encode' => false]);
+            $step_navigation_area .= '</div>';
 
-			// Check if completed tab is set as start_step
-			if ($this->start_step == 'completed') {
-				$class = 'active';
-			}
+            $tab_content .= $step_navigation_area;
 
-			// Add completed icon to wizard line
-			$wizard_line .= Html::tag(
-				'li',
-				Html::a('<span class="round-tab"><i class="glyphicon glyphicon-ok"></i></span>', '#complete', [
-					'data-toggle' => 'tab',
-					'aria-controls' => 'complete',
-					'role' => 'tab',
-					'title' => 'Complete',
-				]),
-				[
-					'role' => 'presentation',
-					'class' => $class,
-					'style' => ['width' => $wizard_line_distribution.'%']
-				]
-			);
+            unset($step_navigation_area);
 
-			$tab_content .= '<div class="tab-pane '.$class.'" role="tabpanel" id="complete">'.$this->complete_content.'</div>';
-		}
+            $tab_content .= $step['content'];
 
-		// Start widget
-		echo '<div class="wizard" id="'.$this->id.'">';
+            // Finish tab
+            $tab_content .= '</div>';
 
-		// Render the steps line
-		echo '<div class="wizard-inner"><div class="connecting-line" style="width:'.$wizard_line_width.'%"></div>';
-		echo '<ul class="nav nav-tabs" role="tablist">'.$wizard_line.'</ul>';
-		echo '</div>';
+            $first = false;
+        }
 
-		// Render the content tabs
-		echo '<div class="tab-content">'.$tab_content.'</div>';
+        // Add a completed step if specified
+        if ($this->complete_content) {
+            $class = 'disabled';
 
-		// Finalize the content tabs
-		echo '<div class="clearfix"></div>';
+            // Check if completed tab is set as start_step
+            if ($this->start_step == 'completed') {
+                $class = 'active';
+            }
 
-		// Finish widget
-		echo '</div>';
-	}
+            // Add completed icon to wizard line
+            $wizard_line .= Html::tag(
+                'li',
+                Html::a($step['title'], '#complete', [
+                    'data-toggle' => 'tab',
+                    'aria-controls' => 'complete',
+                    'role' => 'tab',
+                    'title' => 'Complete',
+                ]),
+                [
+                    'role' => 'presentation',
+                    'class' => $class,
+                    // 'style' => ['width' => $wizard_line_distribution.'%']
+                ]
+            );
 
-	/**
-	 * Generate navigation button
-	 *
-	 * @param string $button_type prev|skip|next\save
-	 * @param array $step step configuration
-	 * @param string $button_id
-	 *
-	 * @return string
-	 */
-	protected function navButton($button_type, $step, $button_id) {
-		// Setup a unique button id
-		$options = ['id' => $button_id.$button_type];
+            $tab_content .= '<div class="tab-pane '.$class.'" role="tabpanel" id="complete">'.$this->complete_content.'</div>';
+        }
 
-		// Apply default button configuration if defined
-		if (isset($this->default_buttons[$button_type]['options'])) {
-			$options = array_merge($options, $this->default_buttons[$button_type]['options']);
-		}
+        // Start widget
+        echo '<div class="wizard" id="'.$this->id.'">';
 
-		// Apply step specific button configuration if defined
-		if (isset($step['buttons'][$button_type]['options'])) {
-			$options = array_merge($options, $step['buttons'][$button_type]['options']);
-		}
+        // Render the steps line
+        $step_line .= '<div class="wizard-inner">';
+        $step_line .= '<header>';
+        $step_line .= '<nav>';
+        $step_line .= '<div class="logo">';
+        $step_line .= Html::a(Html::img('/files/logo/logo.svg', ['alt' => 'Falcoon Logo']), Url::to('/', true));
+        $step_line .= '</div>';
 
-		// Add navigation class
-		if ($button_type == 'prev') {
-			$options['class'] = $options['class'].' prev-step';
-		} else {
-			$options['class'] = $options['class'].' next-step';
-		}
+        echo $step_line;
 
-		// Display button
-		if (isset($step['buttons'][$button_type]['html'])) {
-			return $step['buttons'][$button_type]['html'];
-		} elseif (isset($step['buttons'][$button_type]['title'])) {
-			return Html::button($step['buttons'][ $button_type ]['title'], $options);
-		} else {
-			return Html::button($this->default_buttons[ $button_type ]['title'], $options);
-		}
-	}
+        // Setup name line
+        $name_line .= '<div class="campaign-name">';
+        $name_line .= Html::tag('h2', $this->campaign_name, ['title' => $this->campaign_name]);
+        $name_line .= '</div>';
+
+        echo $name_line;
+
+        echo '<ul class="nav nav-tabs steps" role="tablist">'.$wizard_line.'</ul>';
+
+        $save_campaign .= '<div class="save-campaign">';
+        $save_campaign .= Html::a('Save Campaign', '#save-campaign');
+        $save_campaign .=  '</div>';
+
+        echo $save_campaign;
+
+        echo '</nav>';
+
+        echo '</header>';
+        echo '</div>';
+
+        // Render the content tabs
+        echo '<div class="tab-content">'.$tab_content.'</div>';
+
+        // Finalize the content tabs
+        echo '<div class="clearfix"></div>';
+
+        // Finish widget
+        echo '</div>';
+    }
+
+    /**
+     * Generate navigation button
+     *
+     * @param string $button_type prev|skip|next\save
+     * @param array $step step configuration
+     * @param string $button_id
+     *
+     * @return string
+     */
+    protected function navButton($button_type, $step, $button_id) {
+        // Setup a unique button id
+        $options = ['id' => $button_id.$button_type];
+
+        // Apply default button configuration if defined
+        if (isset($this->default_buttons[$button_type]['options'])) {
+            $options = array_merge($options, $this->default_buttons[$button_type]['options']);
+        }
+
+        // Apply step specific button configuration if defined
+        if (isset($step['buttons'][$button_type]['options'])) {
+            $options = array_merge($options, $step['buttons'][$button_type]['options']);
+        }
+
+        // Add navigation class
+        if ($button_type == 'prev') {
+            $options['class'] = $options['class'].' prev-step previous';
+        } else {
+            $options['class'] = $options['class'].' next-step next';
+        }
+
+        // Display button
+        if (isset($step['buttons'][$button_type]['html'])) {
+            return $step['buttons'][$button_type]['html'];
+        } elseif (isset($step['buttons'][$button_type]['title'])) {
+            return Html::button($step['buttons'][ $button_type ]['title'], $options);
+        } else {
+            return Html::button($this->default_buttons[ $button_type ]['title'], $options);
+        }
+    }
 }
